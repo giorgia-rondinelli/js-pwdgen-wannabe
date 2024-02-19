e1027728f51330c9dbeae2c556692dff2ee26596
giorgia-rondinelli <g.rondinelli4@gmail.com>
2024-02-19 14:51:50 +0100

    [add] password in js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const nome = prompt ('inserisci il nome');
+const cognome = prompt ('inserisci il cognome');
+const colorePreferito = prompt ('inserisci il tuo colore preferito');
+console.log(nome,cognome,colorePreferito);
+
+const password= nome+cognome+colorePreferito+'24';
+console.log(password);
+
